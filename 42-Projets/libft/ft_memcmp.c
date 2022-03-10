85ce3e64d04bdc802a6948a1ee2ca2c244bd33c3
xDeadpoolx <eric.wursteisen@gmail.com>
2022-03-10 16:03:50 -0500

    update libft

--- 42-Projets/libft/ft_memcmp.c
+++ 42-Projets/libft/ft_memcmp.c
@@ -15,11 +15,11 @@
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned int	i;
-	unsigned char	*src1;
-	unsigned char	*src2;
+	const unsigned char	*src1;
+	const unsigned char	*src2;
 
-	src1 = s1;
-	src2 = s2;
+	src1 = (const unsigned char *)s1;
+	src2 = (const unsigned char *)s2;
 	i = 0;
 	while (i < n)
 	{
