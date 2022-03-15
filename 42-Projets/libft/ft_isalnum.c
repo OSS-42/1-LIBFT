85b90d2a9dc8587738a5ca607925c8a80be866f2
xDeadpoolx <eric.wursteisen@gmail.com>
2022-03-15 11:30:34 -0400

    libft update

--- 42-Projets/libft/ft_isalnum.c
+++ 42-Projets/libft/ft_isalnum.c
@@ -10,11 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 int ft_isalnum(int c)
 {
-    if (ft_isalpha(c) == 1 || ft_isdigit(c) == 1)
-        return (1);
-    return (0);
+    if (c < '0' || (c > '9' && c < 'A') || (c > 'Z' && c < 'a') || c > 'z')
+        return (0);
+    return (1);
 }
