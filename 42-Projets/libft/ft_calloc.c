ace5cb2fb599c3925a732a1801f896671af1d4f1
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-06 16:10:08 -0400

    adding ft_calloc.c

---
+++ 42-Projets/libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 16:09:34 by ewurstei          #+#    #+#             */
+/*   Updated: 2022/04/06 16:09:34 by ewurstei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_calloc(size_t count, size_t size)
+{
+    char    *buffer;
+    
+    if (count == 0 || size == 0)
+        return (NULL)    
+    buffer = malloc(count * size);
+    ft_bzero(buffer, count * size);
+    return (buffer);
+}
