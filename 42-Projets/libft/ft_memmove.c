dcef8887d72a5934072eb4b31f9108d25b89f1a7
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-07 10:32:28 -0400

    Update Libft

--- 42-Projets/libft/ft_memmove.c
+++ 42-Projets/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 14:11:20 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/04/05 13:56:54 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/04/07 10:26:46 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char	*buffer;
 
-	buffer = (char *)malloc(sizeof(char) * n);
+	buffer = ft_calloc(1, n);
 	if (buffer == NULL)
 		return (NULL);
 	if (dest == NULL && src == NULL)
