13da7b50a7e6b6288e51f39e1ccf234d3a199e4c
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-07 10:24:30 -0400

    update libft

--- 42-Projets/libft/ft_calloc.c
+++ 42-Projets/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 14:53:26 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/04/05 16:05:07 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/04/07 10:23:39 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*mem;
 
-	if (count == NULL || size == NULL)
-		return (NULL);
 	mem = malloc(count * size);
 	if (mem == NULL)
 		return (NULL);
