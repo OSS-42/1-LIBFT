4b911f28f5f6c4568791a35fb20a9b9e55ec78cd
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-08 13:13:15 -0400

    update libft

--- 42-Projets/libft/ft_strlcat.c
+++ 42-Projets/libft/ft_strlcat.c
@@ -6,11 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 14:41:09 by ewurstei          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/04/08 11:21:07 by ewurstei         ###   ########.fr       */
-=======
-/*   Updated: 2022/04/07 16:24:21 by ewurstei         ###   ########.fr       */
->>>>>>> 11cd5158105e41171e9d0cecbb43c275ad012680
+/*   Updated: 2022/04/08 13:12:40 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +21,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 
 	i = 0;
 	j = ft_strlen(dest);
-<<<<<<< HEAD
 	dlen = j;
-=======
-	dlen = ft_strlen(dest);
->>>>>>> 11cd5158105e41171e9d0cecbb43c275ad012680
 	slen = ft_strlen((char *)src);
 	if (dlen > dstsize)
 		return (dstsize + slen);
