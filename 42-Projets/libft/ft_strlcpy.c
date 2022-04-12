66e8a256e5cbce85efcd7d6ee1d0d74563daa5e4
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-12 09:10:21 -0400

    update libft

--- 42-Projets/libft/ft_strlcpy.c
+++ 42-Projets/libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 10:01:40 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/04/08 13:24:31 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/04/12 09:10:10 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,9 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 
 	slen = ft_strlen((char *)src);
 	i = 0;
-<<<<<<< HEAD
-	if (dstsize > 0)
-=======
 	if (dstsize <= 0)
 		return (slen);
 	while (src[i] != '\0' && i < dstsize - 1)
->>>>>>> 1bcf3237b8723bdfabcb782a037d42f324396db1
 	{
 		while (src[i] != '\0' && i < dstsize - 1)
 		{
