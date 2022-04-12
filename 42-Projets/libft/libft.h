e02a5d1f42238d8b1b974413b4919a81ac1cafde
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-12 16:51:51 -0400

    update libft

--- 42-Projets/libft/libft.h
+++ 42-Projets/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 14:23:32 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/04/12 10:57:14 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/04/12 16:06:29 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
+char			ft_itoa(int n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
