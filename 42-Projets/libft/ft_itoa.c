620e20224ed2a85e4ada2fecb84ec6a2219127f5
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-13 09:45:15 -0400

    update libft

--- 42-Projets/libft/ft_itoa.c
+++ 42-Projets/libft/ft_itoa.c
@@ -6,12 +6,11 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:05:14 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/04/12 22:45:11 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/04/13 09:40:17 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <limits.h>
 
 static int	ft_size(long nb);
 
@@ -19,7 +18,7 @@ int	ft_size(long nb)
 {
 	int	count;
 
-	count = 0;
+	count = 1;
 	if (nb < 0)
 	{
 		nb = nb * -1;
@@ -44,7 +43,7 @@ char	*ft_itoa(int n)
 	dst = (char *)malloc(sizeof(char) * (size + 1));
 	if (!dst)
 		return (NULL);
-	dst[size + 1] = '\0';
+	dst[size] = '\0';
 	if (nb == 0)
 		dst[0] = 48;
 	if (nb < 0)
@@ -54,22 +53,9 @@ char	*ft_itoa(int n)
 	}
 	while (nb > 0)
 	{
-		dst[size] = 48 + nb % 10;
+		dst[size - 1] = 48 + nb % 10;
 		nb = nb / 10;
 		size--;
 	}
 	return (dst);
 }
-
-/*int main (void)
-{
-	int	n1;
-	int	n2;
-
-	n1 = INT_MAX;
-	n2 = INT_MIN;
-	printf("n1 : %d\n", n1);
-	printf("n2 : %d\n", n2);
-	printf("s1 : %s\n", ft_itoa(n1));
-	printf("s2 : %s\n", ft_itoa(n2));
-}*/
