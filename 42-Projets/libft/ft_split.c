f5959143786d447a2f291baa4fa26ae1d623155f
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-13 22:00:08 -0400

    update libft

--- 42-Projets/libft/ft_split.c
+++ 42-Projets/libft/ft_split.c
@@ -12,33 +12,63 @@
 
 #include "libft.h"
 
+/*static void	*ft_wordstorage(char *ptr, char *str, size_t wordlen, size_t i)
+{
+	ptr = (char *)malloc(sizeof(char) * (wordlen + 1));
+	ptr[wordlen + 1] = '\0';
+	while (wordlen > 0)
+	{
+		ptr[wordlen] = str[wordlen + i]; 
+		wordlen--;
+	}
+	return (ptr);
+}
+
+static void	**ft_rowsalloc(void **ptr, size_t size)
+{
+	char	**new_ptr;
+
+	**new_ptr = (char **)malloc(sizeof(char) * size + 1);
+	ft_memcpy(new_ptr, ptr, size);
+	free(ptr);
+	return (new_ptr);
+}
+*/
 static unsigned int	ft_wordcount(char *s, char c)
 {
 	unsigned int	i;
 	unsigned int	wordcount;
+	unsigned int	cycles;
 	unsigned int	wordlen;
 	char			**dst;
 
 	i = 0;
 	wordcount = 0;
 	wordlen = 0;
+	cycles = 0;
 	while (s[i])
 	{
 		if (s[i] && s[i] == c)
 			i++;
 		if (s[i] && s[i] != c)
 		{
-			dst[wordcount][i] = (char **)malloc(sizeof(char) * 2);
-			//malloc au fur et à mesure que le programme lis les caractère du 'mot'.
-			//à chaque nouveau mot, on passe à la row d'après.
+			cycles = wordcount;
 			if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
 				wordcount++;
 			i++;
+			wordlen++;
 			write(1, &s[i - 1], 1);
-			write(1, "\n", 1);
-			
+			if (wordcount > cycles)
+			{
+				write(1, "\n", 1);
+				printf("wordlen : %d\n", wordlen);
+//				dst = ft_rowsralloc(**dst, wordcount);
+//				dst = ft_wordstorage(*dst, s, wordlen, i);
+				wordlen = 0;
+			}
 		}
 	}
+//	return (dst);
 	return (wordcount);
 }
 
@@ -83,11 +113,12 @@ int main (void)
 	printf("wordcount s4 (1) : %u\n", ft_wordcount(s4, c));
 	printf("wordcount s5 (1) : %u\n", ft_wordcount(s5, c));
 
+	return (0);
+}
+
 /*	printf("s1 : %s\n", s1);
 	printf("s2 : %s\n", s2);
 	printf("ch : %c\n", c);
 	printf("fts1 : %s\n", **ft_split(*s1, c));
 	printf("fts2 : %s\n", **ft_split(*s2, c));*/
 	
-	return (0);
-}
