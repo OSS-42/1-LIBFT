e0c67989035736c76e3ba7a2dcb126e8bd3fbf55
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-14 10:38:30 -0400

    update libft

--- 42-Projets/libft/ft_split.c
+++ 42-Projets/libft/ft_split.c
@@ -6,12 +6,13 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 13:24:56 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/04/13 16:29:02 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/04/14 10:38:21 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
 static void	*ft_wordstorage(char *ptr, char *str, size_t wordlen, size_t i)
 {
 	ptr = (char *)malloc(sizeof(char) * (wordlen + 1));
@@ -23,16 +24,19 @@ static void	*ft_wordstorage(char *ptr, char *str, size_t wordlen, size_t i)
 	}
 	return (ptr);
 }
+*/
 
-static char	**ft_rowsalloc(char **ptr, size_t size)
+/*
+static char	**ft_rowsalloc(char *ptr, size_t row)
 {
 	char	**new_ptr;
 
-	**new_ptr = (char **)malloc(sizeof(char) * size);
-	ft_memcpy(new_ptr, ptr, size);
+	new_ptr = (char **)malloc(sizeof(char) * row);
+	ft_memmove(new_ptr, ptr, row);
 	free(ptr);
 	return (new_ptr);
 }
+*/
 
 //static unsigned int	ft_wordcount(char *s, char c)
 char **ft_split(const char *s, char c)
@@ -42,7 +46,8 @@ char **ft_split(const char *s, char c)
 	unsigned int	cycles;
 	unsigned int	wordlen;
 	char			**dst;
-
+	
+	dst = NULL;
 	i = 0;
 	wordcount = 0;
 	wordlen = 0;
@@ -63,43 +68,54 @@ char **ft_split(const char *s, char c)
 			{
 //				write(1, "\n", 1);
 //				printf("wordlen : %d\n", wordlen);
-				dst = ft_rowsalloc(**dst, wordcount);
-				dst = ft_wordstorage(*dst, (char *)s, wordlen, i);
-				wordlen = 0;
+				*dst[wordcount - 1] = (char *)malloc(sizeof(char) * wordcount);
+				dst[wordcount - 1][wordlen + 1] = '\0';
+				while (wordlen > 0)
+				{
+					*dst[wordcount - 1] = (char *)malloc(sizeof(char) * (wordlen + 1));
+					dst[wordcount - 1][wordlen] = s[wordlen + i];
+					wordlen--;
+				}
+//				wordlen = 0;
 			}
 		}
+		*dst[wordcount] = (char *)malloc(sizeof(char) * 1);
+		dst[wordcount][0] = '\0';
 	}
 	return (dst);
 //	return (wordcount);
 }
 
-/*
-char **ft_split(const char *s, char c)
+int main (void)
 {
 	unsigned int	i;
-	unsigned int	size;
-	unsigned int	wordcount;
-	char 			**dst;
 	
-	if (!s)
-		return (NULL);
-
-	wordcount = ft_wordcount((char *)s, c);
+	char **table;
+	char	s1[] = "     Please   Split   Me   !     ";
+	/*char	s2[] = "OSS117 - Alerte Rouge en Afrique Noire !";
+	char	s3[] = "OSS117 ";
+	char	s4[] = " OSS117";
+	char	s5[] = "OSS117";*/
+	char	c = ' ';
 
 	i = 0;
-	while (wordcount > 0)
-	{
-		while (s[i])
-		{
-			
-			i++;
-		}
-		wordcount--;
-	}
 	
-	return (dst);
-}*
+	table = ft_split(s1, c);
+  
+	while (table[i])
+	{
+		printf(" s1 (4) : %s\n", *table);
+		i++;
+	}
+	/*printf(" s2 (8) : %s\n", ft_split(s2, c));
+	printf(" s3 (1) : %s\n", ft_split(s3, c));
+	printf(" s4 (1) : %s\n", ft_split(s4, c));
+	printf(" s5 (1) : %s\n", ft_split(s5, c));*/
 
+	return (0);
+}
+
+/*
 int main (void)
 {
 	char	s1[] = "     Please   Split   Me   !     ";
@@ -119,10 +135,3 @@ int main (void)
 }
 
 */
-
-/*	printf("s1 : %s\n", s1);
-	printf("s2 : %s\n", s2);
-	printf("ch : %c\n", c);
-	printf("fts1 : %s\n", **ft_split(*s1, c));
-	printf("fts2 : %s\n", **ft_split(*s2, c));*/
-	
