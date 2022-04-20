fc43f3d0c1b60137dd6edfc62795ae541e78ef79
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-20 15:36:42 -0400

    update libft

---
+++ 42-Projets/libft/ft_lstadd_back.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 15:27:30 by ewurstei          #+#    #+#             */
+/*   Updated: 2022/04/20 15:36:32 by ewurstei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		ft_lstadd_back(t_list **lst, t_list *new)
+{
+	unsigned int	count;
+	unsigned int	i;
+
+	if (!lst)
+		return ;
+	count = ft_lstsize(new);
+		
+}
+
--- 42-Projets/libft/libft.h
+++ 42-Projets/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 14:23:32 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/04/20 15:17:55 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/04/20 15:26:42 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ t_list		*ft_lstnew(void *content);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
 t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
 //void			ft_putchar(char c);
 //void			ft_putnbr(int nb);
 //void			ft_putstr(char *str);
