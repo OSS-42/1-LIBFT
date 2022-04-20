eb963006c928b236049f9f68f12bec63859635be
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-19 21:56:28 -0400

    update libft

--- 42-Projets/libft/ft_listnew.c
+++ 42-Projets/libft/ft_listnew.c
@@ -16,7 +16,9 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
-	new = (t_list)malloc(sizeof(t_list));
+	new = (t_list*)malloc(sizeof(new));
+	if (!new)
+		return (NULL);
 	new->content = content;
 	new->next = NULL;
 	return (new);
