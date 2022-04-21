edeb4c3de36d437633401f522a67075e94dbc918
xDeadpoolx <eric.wursteisen@gmail.com>
2022-04-20 22:34:42 -0400

    update libft

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -14,12 +14,16 @@
 
 void		ft_lstadd_back(t_list **lst, t_list *new)
 {
-	unsigned int	count;
-	unsigned int	i;
+	t_list *back;
 
-	if (!lst)
-		return ;
-	count = ft_lstsize(new);
-		
+	if (new)
+	{
+		if (*lst)
+		{
+			back = ft_lstlast(*lst);
+			back->next = new;
+		}
+		else
+			*lst = new;
+	}
 }
-
